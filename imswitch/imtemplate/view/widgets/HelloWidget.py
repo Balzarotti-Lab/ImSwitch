5569b677a4997f9e76ef22fac6ba4aee3e46b152
Kubiczek36 <jakub.dokulil@imp.ac.at>
2024-04-26 20:22:03 +0200

    Add logging and update UI in HelloWidget.py

--- imswitch/imtemplate/view/widgets/HelloWidget.py
+++ imswitch/imtemplate/view/widgets/HelloWidget.py
@@ -5,6 +5,7 @@ from qtpy import QtCore, QtWidgets
 # from imswitch.imcontrol.view import guitools
 
 from .basewidgets import Widget
+import logging
 
 
 class HelloWidget(Widget):
@@ -12,10 +13,33 @@ class HelloWidget(Widget):
     def __init__(self, *args, **kwargs):
         # debug log message
         super().__init__(*args, **kwargs)
-        print('HelloWidget.__init__')
-        self.__logger.debug('Initializing')
-        self.label = QtWidgets.QLabel('Hello, World!')
-        self.showCheck = QtWidgets.QCheckbox('show')
-        self.showCheck.setChecked(True)
-        # self.setLayout(QtWidgets.QVBoxLayout())
-        # self.layout().addWidget(self.label)
+        self.__logger = initLogger(self)
+        self.__logger.info('Initializing HelloWidget')
+
+
+        self.showCheck = QtWidgets.QCheckBox("hiiiii")
+        self.showCheck.setCheckable(True)
+
+        # Viewbox
+        self.widget = pg.GraphicsLayoutWidget()
+        self.vb = self.widget.addViewBox(row=1, col=1)
+        self.vb.setMouseMode(pg.ViewBox.RectMode)
+        self.img = pg.ImageItem(axisOrder='row-major')
+        self.img.setTransform(self.img.transform().translate(-0.5, -0.5))
+        self.vb.addItem(self.img)
+        self.b.setAspectLocked (True)
+        self.hist = pg.HistogramLUTItem(image=self.img)
+        self.hist.vb.setLimits(yMin=0, yMax=66000)
+        self.hist.gradient.loadPreset('greyclip')
+        for tick in self.hist.gradient.ticks:
+            tick.hide()
+        self.cwidget.addItem(self.hist, row=1, col=2)
+        # Add elements to GridLayout
+        grid = QtWidgets.QGridLayout()
+        self.setLayout(grid)
+        grid.addWidget(self.cwidget, 0, 0, 1, 6)
+        grid.addWidget(self.showCheck, 1, 0, 1, 1)
+
+        # Connect signals
+        self.showCheck.toggled.connect(self.sigShowToggled)
+        self.layer = None
