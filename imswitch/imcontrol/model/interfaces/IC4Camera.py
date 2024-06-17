6598b0b9e12f74c5f763e3c4b5937aaa938d0585
Jakub Dokulil <jakub.dokulil@imp.ac.at>
2024-06-17 16:10:20 +0200

    objects created #23

---
+++ imswitch/imcontrol/model/interfaces/IC4Camera.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+import imagingcontrol4 as ic4
+from imswitch.imcommon.model import initLogger
+
+
+class IC4Camera:
+    def __init__(self, serial_number=None):
+
+
+        self.__logger = initLogger(self, tryInheritParent=True)
+
+        self.__logger.debug("IC4camera.__init__")
+        ic4.Library.init()
+
+        self.grabber, self.model = self.get_grabber(serial_number)
+
+        # Configure the device to output images in the Mono16 pixel format
+        self.grabber.device_property_map.set_value(ic4.PropId.PIXEL_FORMAT, ic4.PixelFormat.Mono16)
+
+
+    def get_grabber(self, serial_number):
+        grabber = ic4.Grabber()
+
+        self.__logger.debug(f"Available devices: {ic4.DeviceEnum.devices()}")
+
+        for device_info in ic4.DeviceEnum.devices():
+            if device_info.serial == serial_number:
+                model = device_info.model_name
+                grabber.device_open(device_info)
+                return grabber, model
