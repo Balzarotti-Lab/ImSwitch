feccb569ede15ca0e4e812a923e043702338684a
Jakub Dokulil <jakub.dokulil@imp.ac.at>
2024-06-21 10:15:40 +0200

    queue sink implemented though not working #208

--- imswitch/imcontrol/model/managers/detectors/IC4Manager.py
+++ imswitch/imcontrol/model/managers/detectors/IC4Manager.py
@@ -22,6 +22,8 @@ class IC4Manager(DetectorManager):
 
         self._camera = self.getIC4obj(detectorInfo.managerProperties['IC4']['serial_no'])
 
+        self._running = False
+
         fullShape = (self._camera.get_property('Width'),
                      self._camera.get_property('Height'))
 
@@ -37,8 +39,11 @@ class IC4Manager(DetectorManager):
                                             editable=True),
             'brightness': DetectorNumberParameter(group='Misc', value=1, valueUnits='arb.u.',
                                                   editable=True),
-            'frame_rate': DetectorNumberParameter(group='Misc', value=10, valueUnits='fps', editable=True),
+            'AcquisitionFrameRate': DetectorNumberParameter(group='Misc', value=10, valueUnits='fps', editable=True),
         }
+
+        # get the pixel size
+        pixel_size = DetectorNumberParameter(group='Misc', value=6.5, valueUnits='um', editable=True)
 
                 # Prepare actions
         actions = {
@@ -50,7 +55,14 @@ class IC4Manager(DetectorManager):
                     model=self._camera.model, parameters=parameters, actions=actions, croppable=True)
 
         # test to set exposure to 99 ms
-        # self.setParameter('ExposureTime', 99)
+        self.setParameter('ExposureTime', 99)
+
+        # set framerate to 10 fps
+        self.setParameter('AcquisitionFrameRate', 10.0)
+
+        # get the exposure time and AcquisitionFrameRate
+        self.__logger.debug(f"ExposureTime: {self.getParameter('ExposureTime')}")
+        self.__logger.debug(f"AcquisitionFrameRate: {self.getParameter('AcquisitionFrameRate')}")
 
     def setParameter(self, name: str, value):
         """ Set a parameter of the detector and returns the new value. """
@@ -64,10 +76,26 @@ class IC4Manager(DetectorManager):
         value = self._camera.set_property(name, value)
         return value
 
+    def getParameter(self, name: str):
+        """ Get a parameter of the detector. """
+        self.__logger.debug(f"IC4Manager.getParameter: {name}")
+
+        # if name not in self._DetectorManager__parameters:
+        #     raise AttributeError(f'Non-existent parameter "{name}" specified')
+
+        value = self._camera.get_property(name)
+        return value
+
+    @property
     def pixelSizeUm(self):
         """ The pixel size in micrometers, in 3D, in the format
         ``[Z, Y, X]``. Non-scanned ``Z`` set to 1. """
-        pass
+        return [1, 1, 1]
+
+    @property
+    def scale(self):
+        return [1,1]
+
 
     def crop(self, hpos: int, vpos: int, hsize: int, vsize: int) -> None:
         """ Crop the frame read out by the detector. """
@@ -77,7 +105,9 @@ class IC4Manager(DetectorManager):
         """ Returns the frame that represents what the detector currently is
         capturing. The returned object is a numpy array of shape
         (height, width). """
-        pass
+        self.__logger.debug(f"IC4Manager.getLatestFrame with {self._camera.latest_frame.shape}")
+        if self._running:
+            return self._camera.latest_frame
 
     def getChunk(self) -> np.ndarray:
         """ Returns the frames captured by the detector since getChunk was last
@@ -91,13 +121,21 @@ class IC4Manager(DetectorManager):
         frame captured at the time that this function was called. """
         pass
 
-    def startAcquisition(self) -> None:
-        """ Starts image acquisition. """
-        pass
+    def startAcquisition(self, whichAcquisition: str = 'continuous'):
+        if not self._running:
+            self.__logger.debug(f"Starting acquisition: {whichAcquisition}")
+            if whichAcquisition == 'continuous':
+                self._camera.setup_continuous_acquisition()
+            elif whichAcquisition == 'single':
+                self._camera.setup_single_acquisition()
+            self._camera.start_acquisition()
+            self._running = True
 
     def stopAcquisition(self) -> None:
         """ Stops image acquisition. """
-        pass
+        if self._running:
+            self._running = False
+            self._camera.stop_acquisition()
 
     def finalize(self) -> None:
         """ Close/cleanup detector. """
